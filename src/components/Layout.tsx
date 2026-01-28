a68a7c535892bb665a80526c23f220a9ff19b47b
jasenmp <gtm4y5kghx@privaterelay.appleid.com>
2026-01-28 06:21:30 -0600

    Global footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,18 +4,27 @@ import SyncStatus from '@/components/SyncStatus';
 import rsLogo from '@/assets/rs_logo.png';
 
 const Layout = () => {
+	const currentYear = new Date().getFullYear();
+
 	return (
 		<TooltipProvider>
-			<div className="min-h-screen p-6">
-				<header className="mb-8 flex items-center justify-between">
-					<Link to="/">
-						<img src={rsLogo} alt="RS" className="h-16" />
-					</Link>
-					<SyncStatus />
-				</header>
-				<main>
-					<Outlet />
-				</main>
+			<div className="flex min-h-screen flex-col">
+				<div className="flex-1 p-6">
+					<header className="mb-8 flex items-center justify-between">
+						<Link to="/">
+							<img src={rsLogo} alt="RS" className="h-16" />
+						</Link>
+						<SyncStatus />
+					</header>
+					<main>
+						<Outlet />
+					</main>
+				</div>
+				<footer className="bg-rs-blue px-6 py-4 text-white">
+					<p className="text-sm">
+						© {currentYear} RS Technologies Inc. All rights reserved.
+					</p>
+				</footer>
 			</div>
 		</TooltipProvider>
 	);
