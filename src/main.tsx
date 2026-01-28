73e0f439b3f184ea5a87fe6882e1aa2e9d7f213f
jasenmp <gtm4y5kghx@privaterelay.appleid.com>
2026-01-28 16:16:59 -0600

    Adding rtequest for persistent storage

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import './index.css';
 import App from './App.tsx';
 import ErrorBoundary from '@/components/ErrorBoundary.tsx';
 
+// Request persistent storage to reduce eviction risk
+navigator.storage?.persist?.();
+
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<ErrorBoundary>
